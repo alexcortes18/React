67175e67af3003c683abbe8c024d69e6dade5f34
Alejandro Cortes <alex.cortes.i10@gmail.com>
2024-09-09 15:15:42 +0800

    Creating the Timer component

---
+++ 10_Practice_QuizApp/src/Components/QuestionTimer.jsx
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react";
+
+export default function QuestionTimer({ timeout, onTimeout }) {
+    const [remainingTime, setRemainingTime] = useState(timeout);
+
+    // setTimeout needs an useEffect because if not it gets re-executed a lot of times because of setInterval (and
+    // subsequently setRemainingTime) which will update the state "remainingTime" and cause the QuestionTimer to
+    // get re-executed.
+    useEffect(() => {
+        console.log('Setting Timeout')
+        const timer = setTimeout(onTimeout, timeout); //after timeout finishes, the onTimeout function gets executed. Comes as a prop
+
+        return ()=>{
+            clearTimeout(timer);
+        }
+
+    }, [onTimeout, timeout]); // in here we need to add the dependencies (props, states, or values depending on those)
+
+    // in here also another useEffect because setRemainingTime makes the UI change and re-execute QuestionTimer.
+    useEffect(() => {
+        console.log('Setting Interval')
+        const interval = setInterval(() => {
+            setRemainingTime(prevTime => prevTime - 10);
+        }, 10);
+
+        // Cleanup Function:
+        // This gets run BEFORE it runs the useEffect function again, or it gets unmounted from the DOM (QuestionTimer)
+        return () => {
+            clearInterval(interval);
+        };
+    }, []);
+
+    return <progress id="question-time" max={timeout} value={remainingTime}></progress>
+}
