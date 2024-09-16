6e94746418630c5ca427c6087baced2ee3cedc55
Alejandro Cortes <alex.cortes.i10@gmail.com>
2024-09-16 09:10:52 +0800

    1. using navigator to get our own browser location 2. outsourcing fetch() to its own file

--- 13_Http_Requests/src/components/AvailablePlaces.jsx
+++ 13_Http_Requests/src/components/AvailablePlaces.jsx
@@ -1,6 +1,8 @@
 import { useState, useEffect } from 'react';
 import Places from './Places.jsx';
 import ErrorPage from './Error.jsx';
+import { sortPlacesByDistance } from '../loc.js'
+import { fetchAvailablePlaces } from '../http.js'
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [isFetching, setIsFetching] = useState(false);
@@ -10,25 +12,24 @@ export default function AvailablePlaces({ onSelectPlace }) {
   // We need useEffect because if not, will creat an infinite loop because of using setAvailablePlaces 
   // which will render again the component AvailablePlaces.
   useEffect(() => {
-    // fetch('http://localhost:3000/places').then((response) => {
-    //   return response.json()
-    // }).then((resData) => {
-    //   setAvailablePlaces(resData.places);
-    // });
     setIsFetching(true);
     async function fetchPlaces() {
+
       try {
-        const response = await fetch('http://localhost:3000/places2');
-        const resData = await response.json();
-        if (!response.ok) {
-          throw new Error('Failed to fetch places')
-        }
-        setAvailablePlaces(resData.places);
+        const places = await fetchAvailablePlaces();
+
+        // Updating the places by how close they are to our current position using the navigator object from the browser.
+        navigator.geolocation.getCurrentPosition((position)=>{
+          const sortedPlaces = sortPlacesByDistance(places, position.coords.latitude, position.coords.longitude)
+          setAvailablePlaces(sortedPlaces);
+          setIsFetching(false); // We change the location of setIsFetching to here because navigator does not accept
+          // and 'await' does not work with 'navigator'
+        });
       }
       catch (error) { //
         SetError({message: error.message || "Could not fetch places, try again later"});
+        setIsFetching(false);
       }
-      setIsFetching(false);
     }
     fetchPlaces();
   }, []);
