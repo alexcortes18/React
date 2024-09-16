4a6e76440181396111c33d625ae68cee2043bd69
Alejandro Cortes <alex.cortes.i10@gmail.com>
2024-09-16 09:46:24 +0800

    1. finished updateUserPlaces ins http.js 2. App.jsx modified to send 'places' to backend

--- 13_Http_Requests/src/http.js
+++ 13_Http_Requests/src/http.js
@@ -1,8 +1,26 @@
-export async function fetchAvailablePlaces(){
-    const response = await fetch('http://localhost:3000/places');
-    const resData = await response.json();
-    if (!response.ok) {
-      throw new Error('Failed to fetch places')
+export async function fetchAvailablePlaces() {
+  const response = await fetch('http://localhost:3000/places');
+  const resData = await response.json();
+  if (!response.ok) {
+    throw new Error('Failed to fetch places')
+  }
+  return resData.places
+}
+
+export async function updateUserPlaces(places) {
+  //response from backend
+  const response = await fetch('http://localhost:3000/user-places', {
+    method: 'PUT',
+    body: JSON.stringify({places}),
+    headers: {
+      'Content-Type': 'application/json' //important to tell the server that we want to send json type object.
     }
-    return resData.places
+  });
+
+  if (!response.ok) {
+    throw new Error('Failed to fetch places')
+  }
+
+  const resData = await response.json();
+  return resData.message;
 }
