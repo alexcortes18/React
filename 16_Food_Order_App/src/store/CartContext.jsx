2fd08f0c3649da65ed7cb8d60306ac465eba2169
Alejandro Cortes <alex.cortes.i10@gmail.com>
2024-09-22 13:32:10 +0800

    added RemoveItem logic to useReducer and the cartCtx.

--- 16_Food_Order_App/src/store/CartContext.jsx
+++ 16_Food_Order_App/src/store/CartContext.jsx
@@ -6,6 +6,8 @@ const CartContext = createContext({
     removeItem: (id) => { }
 });
 
+// the state is provided by REACT in useReducer, but the action is usually an {} object, we provide in our functions,
+// which should contain "type" and "item" in this case: {type: ..., item: ...}, or {type: ..., id: ...}, etc...
 function cartReducer(state, action) {
     if (action.type === 'ADD_ITEM') {
         // Find if the item already has been added for the first time.
@@ -25,23 +27,55 @@ function cartReducer(state, action) {
             updatedItems.push({ ...action.item, quantity: 1 });
         }
 
-        return {...state, items: updatedItems}
+        return { ...state, items: updatedItems }
     }
     if (action.type === 'REMOVE_ITEM') {
-        // ... remove an item
+        const existingCartItemIndex = state.items.findIndex(
+            (item) => item.id === action.id);
+        const existingCartItem = state.items[existingCartItemIndex];
+
+        const updatedItems = [...state.items];
+        if (existingCartItem.quantity === 1) {
+            updatedItems.splice(existingCartItemIndex, 1); //remove 1 item from the index existingCartItemIndex.
+        } else {
+            const updatedItem = {
+                ...existingCartItem,
+                quantity: existingCartItem.quantity - 1
+            }
+            updatedItems[existingCartItemIndex] = updatedItem;
+        }
+        return{
+            ...state,
+            items: updatedItems
+        }
+
     }
     return state;
 }
 
 export function CartContextProvider({ children }) {
-    useReducer(cartReducer,
+    const [cart, dispatchCartAction] = useReducer(cartReducer,
         // Everything in here is the state that useReducer provides to us: (in this case we only have items array)
         {
             items: []
         });
 
+        
+        function addItem(item){
+            dispatchCartAction({type: 'ADD_ITEM', item: item})
+        }
+        
+        function removeItem(id){
+            dispatchCartAction({type: 'REMOVE_ITEM', id: id})
+        }
+        
+        const cartCtx = {
+            items: cart.items,
+            addItem, // since they have the same name we can avoid-> addItem: addItem.
+            removeItem,
+        }
 
-    return <CartContext.Provider>{children}</CartContext.Provider>
+    return <CartContext.Provider value={cartCtx}>{children}</CartContext.Provider>
 }
 
 export default CartContext;
