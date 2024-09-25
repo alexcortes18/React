9dd0c8a8ee951ec1c200fe8ae745e94cd247abe2
Alejandro Cortes <alex.cortes.i10@gmail.com>
2024-09-25 10:43:59 +0800

    Submitting the form and sending a POST request to our dummy backend

--- 16_Food_Order_App/src/components/Checkout.jsx
+++ 16_Food_Order_App/src/components/Checkout.jsx
@@ -14,19 +14,41 @@ export default function Checkout() {
         return totalPrice + item.quantity * item.price;
     }, 0)
 
-    return <Modal open={userProgressCtx.progress === 'checkout'} onClose={()=> userProgressCtx.hideCheckout()}>
-        <form>
+    function handleSubmit(event) {
+        event.preventDefault();
+
+        const fd = new FormData(event.target);
+        const customerData = Object.fromEntries(fd.entries());
+
+        fetch('http://localhost:3000/orders',{
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                order: {
+                    items: cartCtx.items,
+                    customer: customerData //uses the data from the form from all inputs with the "name" object. The <Input> has 
+                    // the 'name' incoming from the id prop.
+                }
+            })
+        });
+    }
+
+    return <Modal open={userProgressCtx.progress === 'checkout'} onClose={() => userProgressCtx.hideCheckout()}>
+        <form onSubmit={handleSubmit}>
             <h2>Checkout</h2>
             <p>Total Amount: {currencyFormatter.format(cartTotalPrice)}</p>
-            <Input label="Full Name" type="text" id="full-name" />
+
+            <Input label="Full Name" type="text" id="name" />
             <Input label="E-Mail Address" type="email" id="email" />
             <Input label="Street" type="text" id="street" />
             <div className="control-row">
                 <Input label="Postal Code" type="text" id="postal-code" />
-                <Input label="City" type="text" id="city"/>
+                <Input label="City" type="text" id="city" />
             </div>
             <p className="modal-actions">
-                <Button type="button" textOnly onClick={()=> userProgressCtx.hideCheckout()}>Close</Button>
+                <Button type="button" textOnly onClick={() => userProgressCtx.hideCheckout()}>Close</Button>
                 <Button>Submit Order</Button>
             </p>
         </form>
