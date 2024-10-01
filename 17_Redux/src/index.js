589cadc38d7d876ee1c20d992acc1adec239183e
Alejandro Cortes <alex.cortes.i10@gmail.com>
2024-10-01 21:14:42 +0800

    Creating our Redux store and providing it to our App.js

--- 17_Redux/src/index.js
+++ 17_Redux/src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux' // to tell our app who cant get access to the Redux 'store'
 
 import './index.css';
 import App from './App';
+import store from './store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(<Provider store={store}><App /></Provider>);
