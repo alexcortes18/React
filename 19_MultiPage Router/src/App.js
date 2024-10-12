eb1089fc3ddf4bdcaa95dc31c9f1fedd40ee7509
Alejandro Cortes <alex.cortes.i10@gmail.com>
2024-10-12 23:37:02 +0800

    Getting started with Routing with React

--- 19_MultiPage Router/src/App.js
+++ 19_MultiPage Router/src/App.js
@@ -1,5 +1,14 @@
+import { createBrowserRouter, RouterProvider} from 'react-router-dom'
+import HomePage from './pages/Home';
+
+const router = createBrowserRouter([
+  // each object is a path to be defined.
+  // Path: which path we want, and element: which component to show.
+  {path:'/', element: <HomePage/>},
+])
+
 function App() {
-  return <div></div>;
+  return <RouterProvider router={router}/>;
 }
 
 export default App;
