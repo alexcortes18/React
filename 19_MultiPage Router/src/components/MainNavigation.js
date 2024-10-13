92faf520dbc746f1ea0d22fa43b406d919926d31
Alejandro Cortes <alex.cortes.i10@gmail.com>
2024-10-13 18:30:53 +0800

    using NavLink instead of Link

--- 19_MultiPage Router/src/components/MainNavigation.js
+++ 19_MultiPage Router/src/components/MainNavigation.js
@@ -1,4 +1,5 @@
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom"; // we will use NavLink instead
+import { NavLink } from "react-router-dom";
 import classes from './MainNavigation.module.css'
 
 export default function MainNavigation() {
@@ -7,10 +8,24 @@ export default function MainNavigation() {
             <nav>
                 <ul className={classes.list}>
                     <li>
-                        <Link to="/">Home</Link>
+                        <NavLink to="/"
+                            className={({ isActive }) => isActive ? classes.active : undefined}
+                            // className is not the normal className for css, but in this case, rather a special prop
+                            // for Navlink that takes a function that returns a class name that should be returned to
+                            // the anchor <a> tag. It also provides us with an object, which we can destructure to get
+                            // the property isActive (provided by React Router Dom) and acts like a boolean if its
+                            // active.
+                            end // Not needed anymore, but for previous versions is to tell React Router Dom, that the path
+                        // "/" is the end of this Navlink and only consider "isActive" if it ends ONLY with "/". Before
+                        // this it would consider /, and /products because "/" is also present in "/products". (Of course
+                        // if we had a NavLink in /products.)
+                        >Home</NavLink>
+
                     </li>
                     <li>
-                        <Link to="/products">Products</Link>
+                        <NavLink to="/products"
+                            className={({ isActive }) => isActive ? classes.active : undefined}
+                        >Products</NavLink>
                     </li>
                 </ul>
             </nav>
