71b0712ef5df33a345cae56332e84fec2832e152
Alejandro Cortes <alex.cortes.i10@gmail.com>
2024-10-24 12:57:30 +0800

    events json

--- 20_Routing_Project/frontend/src/pages/EventDetailPage.js
+++ 20_Routing_Project/frontend/src/pages/EventDetailPage.js
@@ -15,6 +15,7 @@ function EventDetailPage() {
 
 export default EventDetailPage;
 
+// TO GET data.
 export async function loader({ request, params }) {
     const id = params.eventId; // React router provides us with 'request' and 'params' objects
     // request -> could be used to access url (request.url)
@@ -23,10 +24,7 @@ export async function loader({ request, params }) {
     // we cannot access the ':eventId' set in the path of EventDetailPage path, because hooks (useParams) cannot
     // be used in here. so we need the params object.
 
-    const response = await fetch('http://localhost:8080/events/' + id, {
-        method: request.method // we could harcoded 'delete', but we can also receive it from the submitted approach
-        // from the <EventItem> component.
-    });
+    const response = await fetch('http://localhost:8080/events/' + id);
     if (!response.ok) {
         throw json({ message: "Could not fetch details for selected event" }, {
             status: 500
@@ -36,10 +34,13 @@ export async function loader({ request, params }) {
     }
 }
 
-
+// TO DELETE data.
 export async function action({ request, params }) {
     const eventId = params.eventId;
-    const response = await fetch('http://localhost:8080/events/' + eventId);
+    const response = await fetch('http://localhost:8080/events/' + eventId,  {
+        method: request.method // we could harcoded 'DELETE', but we can also receive it from the submitted approach
+        // from the <EventItem> component.
+    });
     if (!response.ok) {
         throw json({ messae: 'Could not delete event' }, { status: 500 });
     }
