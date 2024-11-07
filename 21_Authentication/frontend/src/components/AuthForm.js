afb7d446825dd1d535aed87d5f87ace0620d0c91
Alejandro Cortes <alex.cortes.i10@gmail.com>
2024-11-07 23:23:29 +0800

    auth action function, and query parameters

--- 21_Authentication/frontend/src/components/AuthForm.js
+++ 21_Authentication/frontend/src/components/AuthForm.js
@@ -1,14 +1,13 @@
-import { useState } from 'react';
-import { Form } from 'react-router-dom';
+import { Form, Link, useSearchParams } from 'react-router-dom';
 
 import classes from './AuthForm.module.css';
 
 function AuthForm() {
-  const [isLogin, setIsLogin] = useState(true);
-
-  function switchAuthHandler() {
-    setIsLogin((isCurrentlyLogin) => !isCurrentlyLogin);
-  }
+  // Query parameters are a way to pass additional information to a URL in the form of key-value pairs, 
+  // which come after a question mark (?) in the URL. They’re commonly used to indicate a specific state or 
+  // option without changing the page’s path.
+  const [searchParams, setSearchParams] = useSearchParams(); // this is for Query Parameters
+  const isLogin = searchParams.get('mode') === 'login'; // we are searching the query parameter of the URL. (?)
 
   return (
     <>
@@ -23,9 +22,9 @@ function AuthForm() {
           <input id="password" type="password" name="password" required />
         </p>
         <div className={classes.actions}>
-          <button onClick={switchAuthHandler} type="button">
+          <Link to={`?mode=${isLogin? 'signup' : 'login'}`}>
             {isLogin ? 'Create new user' : 'Login'}
-          </button>
+          </Link>
           <button>Save</button>
         </div>
       </Form>
--- 21_Authentication/frontend/src/pages/Authentication.js
+++ 21_Authentication/frontend/src/pages/Authentication.js
@@ -1,3 +1,4 @@
+import { json, redirect } from 'react-router-dom';
 import AuthForm from '../components/AuthForm';
 
 function AuthenticationPage() {
@@ -5,3 +6,57 @@ function AuthenticationPage() {
 }
 
 export default AuthenticationPage;
+
+export async function action({ request }) {
+  /*
+    This function here handles the form submittion (from <Form>) from the <AuthForm>. We are doing this to validate
+    that the user exists.
+
+    Steps:
+    1. Get the query parameters (?mode) from the URL.
+    2. Get the data from the Form (request.formData())
+    3. Create the object (authData) to send as response data to the backend.
+    4. Send it with 'await fetch()'
+    5. If we get a status response from backend as errors we send those forward to the frontend.
+    6. If we are succesful with validating the user/pass, we redirect the user to the homepage "/"
+  */
+
+
+  const searchParams = new URL(request.url).searchParams; // this is a way to get the searchParams from the URL
+  // given that we cannot use useSearchParams in here
+
+  const mode = searchParams.get('mode') || 'login';
+
+  if(mode !== 'login' && mode !== 'signup'){
+    throw json({message: "Unsupported Mode"}, {status: 422});
+  }
+
+  const data = await request.formData();
+  const authData = {
+    email: data.get('email'),
+    password: data.get('password')
+  };
+
+  const response = await fetch('http://localhost:8080/' + mode, {
+    method: "POST",
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(authData),
+  });
+
+
+  // Some possible errors to send forward
+  if (response.status === 422 || response.status === 401){
+    return response;
+  }
+
+  if (!response.ok){
+    throw json({message: 'Could not authenticate user'}, {stauts: 500})
+  }
+
+  // soon: manage the token from the backend.
+
+  return redirect('/');
+
+}
