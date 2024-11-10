45ae8b90786026bb3c7db733fb266619c5e59ef9
Alejandro Cortes <alex.cortes.i10@gmail.com>
2024-11-10 20:35:44 +0800

    adding loader to check if authentication of token is ok, so user cannot type the /new or /edit in the URL and access it by himself

--- 21_Authentication/frontend/src/util/auth.js
+++ 21_Authentication/frontend/src/util/auth.js
@@ -1,3 +1,5 @@
+import { redirect } from 'react-router-dom';
+
 export function getAuthToken(){
     const token = localStorage.getItem('token');
     return token;
@@ -5,4 +7,15 @@ export function getAuthToken(){
 
 export function tokenLoader(){
     return getAuthToken();
+}
+
+export function checkAuthLoader(){
+    const token = getAuthToken();
+
+    if(!token){
+        return redirect('/auth');
+    }
+
+    return null; // a loader SHOULD ALWAYS return something. If the previous if was not met, then it returns nothing, which
+    // is wrong. Loaders are expected to return a value.
 }
